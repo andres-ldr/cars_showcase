91961e3bd6b5dc62f4a3d055449d7646125f7bd5
Andrés <lopez.andresrigoberto@gmail.com>
2023-12-25 20:27:15 -0800

    add: btn type prop interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,5 +3,6 @@ import { MouseEventHandler } from 'react';
 export interface CustomButtomProps {
   title: string;
   containerStyles?: string;
-  handleClick: MouseEventHandler<HTMLButtonElement>;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: 'button' | 'submit' | 'reset';
 }
