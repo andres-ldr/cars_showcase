46921f5d59f5688af843f383bbe9a1a1ae9de368
Andrés <lopez.andresrigoberto@gmail.com>
2023-12-27 23:20:57 -0800

    add: car card and car details to export file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,21 @@
 import SearchManufacturer from './SearchManufacturer';
 import CustomFilter from './CustomFilter';
 import CustomButton from './CustomButton';
+import CarDetails from './CarDetails';
 import SearchBar from './SearchBar';
+import CarCard from './CarCard';
 import Footer from './Footer';
 import Navbar from './Navbar';
 import Hero from './Hero';
 
-
-export { Hero, CustomButton, CustomFilter, Footer, SearchBar, Navbar, SearchManufacturer };
+export {
+  SearchManufacturer,
+  CustomButton,
+  CustomFilter,
+  CarDetails,
+  SearchBar,
+  CarCard,
+  Footer,
+  Navbar,
+  Hero,
+};
