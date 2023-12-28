43aca8f0dc7fc20690a0120ad7b84bc0800c206e
Andrés <lopez.andresrigoberto@gmail.com>
2023-12-27 23:17:24 -0800

    add: fetch cars from api and calculate rent

---
+++ src/utils/index.ts
@@ -0,0 +1,31 @@
+export async function fetchCars() {
+  const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'fa699073f0msh2b4850b8c2c4189p1b1e02jsn7e3e29aa0333',
+      'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50;
+  const mileageFactor = 0.1;
+  const ageFactor = 0.05;
+
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
